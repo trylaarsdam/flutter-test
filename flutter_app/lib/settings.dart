88db5b31d9b4b4b844328303b0ca091788a9cc35
Todd Rylaarsdam <50586094+trylaarsdam@users.noreply.github.com>
2020-08-12 14:12:57 -0500

    emails working, ble updates

--- flutter_app/lib/settings.dart
+++ flutter_app/lib/settings.dart
@@ -7,11 +7,19 @@ import 'package:typed_data/typed_data.dart' as typed;
 import './main.dart' as mainDart;
 import 'package:flutter_blue/flutter_blue.dart';
 import 'dart:convert';
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server.dart';
+
+String username = 'rylaarsdamto@gmail.com';
+String password = 'faucet@3am';
+
+var settingsGlobal;
 
 class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Container(child: new Settings());
+    settingsGlobal = new Container(child: new Settings());
+    return settingsGlobal;
   }
 }
 
@@ -20,6 +28,27 @@ class Settings extends StatefulWidget {
 
   @override
   _SettingsState createState() => _SettingsState();
+}
+
+void sendEmail(address) async {
+  final smtpServer = gmail(username, password);
+  final email = Message()
+    ..from = Address(username, 'CPU Connect')
+    ..recipients.add(address)
+    ..subject = 'CPU Connect UUID'
+    ..text =
+        "Here's your UUID: \n${mainDart.uuid} \n \nSent on ${DateTime.now()}";
+  try {
+    final sendReport = await send(email, smtpServer);
+    print('Message sent: ' + sendReport.toString());
+
+  } catch (e) {
+    print('Message not sent.');
+    print(e);
+    for (var p in e.problems) {
+      print('Problem: ${p.code}: ${p.msg}');
+    }
+  }
 }
 
 class _SettingsState extends State<Settings>
@@ -39,13 +68,8 @@ class _SettingsState extends State<Settings>
             );
           },
         ),
-        body: new Semantics(
-            container: true,
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Switch(onChanged: switchChanged, value: isSwitched)
-                ])));
+        body: new EmailForm()
+        );
   }
 
   void switchChanged(bool value) {
@@ -58,6 +82,8 @@ class _SettingsState extends State<Settings>
           buff[0] = 'o'.codeUnitAt(0);
           buff[1] = 'n'.codeUnitAt(0);
           mainDart.client.publishMessage(topic, MqttQos.atLeastOnce, buff);
+          final message = "";
+          sendEmail(message);
         } else {
           var topic = mainDart.uuid + '/settings';
           final buff = typed.Uint8Buffer(3);
@@ -74,6 +100,79 @@ class _SettingsState extends State<Settings>
   bool get wantKeepAlive => true;
 }
 
+// Create a Form widget.
+class EmailForm extends StatefulWidget {
+  @override
+  EmailFormState createState() {
+    return EmailFormState();
+  }
+}
+
+// Create a corresponding State class.
+// This class holds data related to the form.
+class EmailFormState extends State<EmailForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a GlobalKey<FormState>,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+  final formController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    formController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Email',
+            ),
+            controller: formController,
+            validator: (value) {
+              bool emailValid = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value);
+              if (value.isEmpty) {
+                return 'Enter your email';
+              }
+              else if(!emailValid){
+                return 'Not a valid email';
+              }
+              else{
+                return null;
+              }
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false
+                // otherwise.
+                if (_formKey.currentState.validate()) {
+                  // If the form is valid, display a Snackbar.
+                  sendEmail(formController.text);
+                  Scaffold.of(context)
+                      .showSnackBar(SnackBar(content: Text('Sending UUID to email')));
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 class BluetoothConnect extends StatefulWidget {
   BluetoothConnect({Key key, this.title}) : super(key: key);
 
@@ -122,35 +221,34 @@ class _BluetoothConnectState extends State<BluetoothConnect> {
       }
     });
 
-      widget.flutterBlue.startScan().catchError( (error) {
-        // set up the buttons
-        Widget cancelButton = FlatButton( 
-          child: Text("Cancel"),
-          onPressed: () {
-            Navigator.pop(context);
-            Navigator.pop(context);
-          },
-        );
+    widget.flutterBlue.startScan().catchError((error) {
+      // set up the buttons
+      Widget cancelButton = FlatButton(
+        child: Text("Cancel"),
+        onPressed: () {
+          Navigator.pop(context);
+          Navigator.pop(context);
+        },
+      );
 
-        // set up the AlertDialog
-        AlertDialog alert = AlertDialog(
-          title: Text("CPU Connect"),
-          content: Text(
-              "This device doesn't have bluetooth enabled or CPU Connect doesn't have permission to use bluetooth. Try enabling bluetooth and restarting the app."),
-          actions: [
-            cancelButton,
-          ],
-        );
+      // set up the AlertDialog
+      AlertDialog alert = AlertDialog(
+        title: Text("CPU Connect"),
+        content: Text(
+            "This device doesn't have bluetooth enabled or CPU Connect doesn't have permission to use bluetooth. Try enabling bluetooth and restarting the app."),
+        actions: [
+          cancelButton,
+        ],
+      );
 
-        // show the dialog
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return alert;
-          },
-        );
-      }
-    );
+      // show the dialog
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alert;
+        },
+      );
+    });
   }
 
   ListView _buildListViewOfDevices() {
